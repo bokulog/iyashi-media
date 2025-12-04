6779bfc5a955992ccf306b837bcd6922d1481865
bokulog <bokunilogin@gmail.com>
2025-12-04 23:05:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const items = document.querySelectorAll('.reveal');
+const targets = document.querySelectorAll('.reveal');
 
-const observer = new IntersectionObserver(entries => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('fade-in');
+      entry.target.classList.add('show');
     }
   });
-});
+}, { threshold: 0.2 });
 
-items.forEach(item => {
-  observer.observe(item);
-});
+targets.forEach(target => observer.observe(target));
